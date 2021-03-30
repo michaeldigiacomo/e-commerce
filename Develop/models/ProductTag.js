b9804030298ac45eed59955418c3b1912bfebb7c
Michael DiGiacomo <mdigiacomo05@me.com>
2021-03-29 21:25:57 -0400

    completed product tag model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
@@ -12,13 +12,27 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
